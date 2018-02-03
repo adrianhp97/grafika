8706d9fd66616648903affd97fae83ca95f45c94
adrianhp97 <13515091@std.stei.itb.ac.id>
2018-02-03 21:15:14 +0700

    add line function

--- Tugas3/line.c
+++ Tugas3/line.c
@@ -1,20 +1,43 @@
 #include "line.h"
 
-void line(Line line) {
-   int dx = abs(line.x1-line.x0), sx = line.x0<line.x1 ? 1 : -1;
-   int dy = abs(line.y1-line.y0), sy = line.y0<line.y1 ? 1 : -1; 
-   int err = (dx>dy ? dx : -dy)/2, e2;
-  
-   for(;;){
-     setPixel(line.x0,line.y0);
-     if (line.x0==line.x1 && line.y0==line.y1) break;
-     e2 = err;
-     if (e2 >-dx) { err -= dy; line.x0 += sx; }
-     if (e2 < dy) { err += dx; line.y0 += sy; }
-   }
- } 
+void setLine(Line *line) {
+  int dx = abs((*line).x1-(*line).x0), sx = (*line).x0<(*line).x1 ? 1 : -1;
+  int dy = abs((*line).y1-(*line).y0), sy = (*line).y0<(*line).y1 ? 1 : -1; 
+  int err = (dx>dy ? dx : -dy)/2, e2;
 
+  for(;;){
+    // setPixel(line.x0,line.y0);
+    if ((*line).x0==(*line).x1 && (*line).y0==(*line).y1) break;
+    e2 = err;
+    if (e2 >-dx) { err -= dy; (*line).x0 += sx; }
+    if (e2 < dy) { err += dx; (*line).y0 += sy; }
+  }
+}
 
-void setPixel(int x, int y) {
-  
+Coodinate getPoint1(Line line) {
+  Coodinate point;
+  setCoordinate(&point, line.x0, line.y0);
+  return point;
+}
+
+Coodinate getPoint2(Line line) {
+  Coodinate point;
+  setCoordinate(&point, line.x1, line.y1);
+  return point;
+}
+
+int getX0(Line line) {
+  return line.x0;
+}
+
+int getX1(Line line) {
+  return line.x1;
+}
+
+int getY0(Line line) {
+  return line.y0;
+}
+
+int getY1(Line line) {
+  return line.y1;
 }
--- Tugas3/line.h
+++ Tugas3/line.h
@@ -4,8 +4,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "struct.h"
+#include "coodinate.h"
 
-void line(Line line);
-void setPixel(int x, int y);
+void setLine(Line *line);
+Coodinate getPoint1(Line line);
+Coodinate getPoint2(Line line);
+int getX0(Line line);
+int getX1(Line line);
+int getY0(Line line);
+int getY1(Line line);
 
 #endif
