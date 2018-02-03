9a1a026708ede4df821172245dff637bc92e04ac
adrianhp97 <13515091@std.stei.itb.ac.id>
2018-02-03 21:04:31 +0700

    add function in color

--- Tugas3/main.c
+++ Tugas3/main.c
@@ -1,12 +1,3 @@
-/*
-To test that the Linux framebuffer is set up correctly, and that the device permissions
-are correct, use the program below which opens the frame buffer and draws a gradient-
-filled red square:
-retrieved from:
-Testing the Linux Framebuffer for Qtopia Core (qt4-x11-4.2.2)
-http://cep.xor.aps.anl.gov/software/qt4-x11-4.2.2/qtopiacore-testingframebuffer.html
-*/
-
 #include <stdlib.h>
 #include <stdio.h>
 #include "struct.h"
@@ -22,8 +13,6 @@ int main()
     frame.fbfd = 0;
     frame.screensize = 0;
     frame.fbp = 0;
-
-    // long int location = 0;
 
     initiateReading(&frame.fbfd, &frame.vinfo, &frame.finfo);
     frame.screensize = getScreenSize(frame.vinfo, frame.finfo);
