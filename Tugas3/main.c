e8cf15eefc65a241ea61f31e1301351e074a7b4d
adrianhp97 <13515091@std.stei.itb.ac.id>
2018-02-03 18:35:16 +0700

    base algorithm

--- Tugas3/main.c
+++ Tugas3/main.c
@@ -24,34 +24,10 @@ int main()
 
     initiateReading(&fbfd, &vinfo, &finfo);
     screensize = getScreenSize(vinfo, finfo);
-    // printf("%dx%d, %dbpp\n", vinfo.xres, vinfo.yres, vinfo.bits_per_pixel);
-    // mapDeviceToMemory(*fbp, &fbfd, screensize);
     fbp = mapDeviceToMemory(&fbfd, screensize);
 
-    x = 100; y = 100;       // Where we are going to put the pixel
+    // set memory here
 
-    // Figure out where in memory to put the pixel
-    for (y = 100; y < 300; y++)
-    for (x = 100; x < 300; x++) {
-
-        location = (x+vinfo.xoffset) * (vinfo.bits_per_pixel/8) +
-                   (y+vinfo.yoffset) * finfo.line_length;
-
-        if (vinfo.bits_per_pixel == 32) {
-            *(fbp + location) = 100;        // Some blue
-            *(fbp + location + 1) = 15+(x-100)/2;     // A little green
-            *(fbp + location + 2) = 200-(y-100)/5;    // A lot of red
-            *(fbp + location + 3) = 0;      // No transparency
-    //location += 4;
-        } else  { //assume 16bpp
-            int b = 10;
-            int g = (x-100)/6;     // A little green
-            int r = 31-(y-100)/16;    // A lot of red
-            unsigned short int t = r<<11 | g << 5 | b;
-            *((unsigned short int*)(fbp + location)) = t;
-        }
-
-    }
     unmapped(fbp, &screensize);
     closeReading(&fbfd);
     return 0;
