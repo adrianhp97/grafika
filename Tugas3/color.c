575a3da7a103a6af5973089e1e5f4ea4d16f3a5a
adrianhp97 <13515091@std.stei.itb.ac.id>
2018-02-03 20:58:12 +0700

    add function in color

--- Tugas3/color.c
+++ Tugas3/color.c
@@ -2,7 +2,21 @@
 
 
 RGBa getColor(FrameBuffer frame, Coordinate point) {
+  RGBa color;
+  long int location = getLocation(frame, point);
+  int r = *(frame.fbp + location + 2);
+  int g = *(frame.fbp + location + 1);
+  int b = *(frame.fbp + location);
+  int gamma = *(frame.fbp + location + 3);
+  setColor(&color, r, g, b ,gamma);
+  return color;
+}
 
+void setColor(RGBa *color, int r, int g, int b, int gamma) {
+  (*color).r = r;
+  (*color).g = g;
+  (*color).b = b;
+  (*color).gamma = gamma;
 }
 
 void paintPixel(char * fbp, long int location, RGBa color) {
@@ -12,32 +26,51 @@ void paintPixel(char * fbp, long int location, RGBa color) {
   *(fbp + location + 3) = color.gamma;      // No transparency
 }
 
-bool isEqualColor(RGBa color1, RGBa color2) {
+bool isEqualR(int r1, int r2) {
+  return (r1 == r2);
+}
 
+bool isEqualG(int g1, int g2) {
+  return (g1 == g2);
+}
+
+bool isEqualB(int b1, int b2) {
+  return (b1 == b2);
+}
+
+bool isEqualGamma(int gamma1, int gamma2) {
+  return (gamma1 == gamma2);
+}
+
+
+bool isEqualColor(RGBa color1, RGBa color2) {
+  return isEqualR(color1.r, color2.r) && isEqualG(color1.g, color2.g) && isEqualB(color1.b, color2.b) && isEqualGamma(color1.gamma, color2.gamma);
 }
 
 void BFill4Connected(FrameBuffer frame, Coordinate point, RGBa fillcoll, RGBa boundcol) {
   RGBa currcol = getColor(frame, point);
   if(!isEqualColor(currcol, fillcoll) && !isEqualColor(currcol, boundcol)) {
-    paintPixel();
-    BFill4Connected(frame, point, fillcoll, boundcol);
-    BFill4Connected(frame, point, fillcoll, boundcol);
-    BFill4Connected(frame, point, fillcoll, boundcol);
-    BFill4Connected(frame, point, fillcoll, boundcol);
+    long int location = getLocation(frame, point);
+    paintPixel(frame.fbp, location, fillcoll);
+    BFill4Connected(frame, getTopCoordinate(point), fillcoll, boundcol);
+    BFill4Connected(frame, getBottomCoordinate(point), fillcoll, boundcol);
+    BFill4Connected(frame, getLeftCoordinate(point), fillcoll, boundcol);
+    BFill4Connected(frame, getRightCoordinate(point), fillcoll, boundcol);
   }
 }
 
 void BFill8Connected(FrameBuffer frame, Coordinate point, RGBa fillcoll, RGBa boundcol) {
   RGBa currcol = getColor(frame, point);
   if(!isEqualColor(currcol, fillcoll) && !isEqualColor(currcol, boundcol)) {
-    paintPixel();
-    BFill8Connected(frame, point, fillcoll, boundcol);
-    BFill8Connected(frame, point, fillcoll, boundcol);
-    BFill8Connected(frame, point, fillcoll, boundcol);
-    BFill8Connected(frame, point, fillcoll, boundcol);
-    BFill8Connected(frame, point, fillcoll, boundcol);
-    BFill8Connected(frame, point, fillcoll, boundcol);
-    BFill8Connected(frame, point, fillcoll, boundcol);
-    BFill8Connected(frame, point, fillcoll, boundcol);
+    long int location = getLocation(frame, point);
+    paintPixel(frame.fbp, location, fillcoll);
+    BFill8Connected(frame, getTopCoordinate, fillcoll, boundcol);
+    BFill8Connected(frame, getBottomCoordinate, fillcoll, boundcol);
+    BFill8Connected(frame, getLeftCoordinate, fillcoll, boundcol);
+    BFill8Connected(frame, getRightCoordinate, fillcoll, boundcol);
+    BFill8Connected(frame, getTopLeftCoordinate, fillcoll, boundcol);
+    BFill8Connected(frame, getBottomLeftCoordinate, fillcoll, boundcol);
+    BFill8Connected(frame, getTopRightCoordinate, fillcoll, boundcol);
+    BFill8Connected(frame, getBottomRightCoordinate, fillcoll, boundcol);
   }
 }
--- Tugas3/color.h
+++ Tugas3/color.h
@@ -2,9 +2,15 @@
 #define COLOR_H
 
 #include "struct.h"
+#include "coordinate.h"
 
 RGBa getColor(FrameBuffer frame, Coordinate point);
+void setColor(FrameBuffer frame, Coordinate point);
 int paintPixel(FrameBuffer frame, long int location, RGBa color);
+bool isEqualR(int r1, int r2);
+bool isEqualG(int g1, int g2);
+bool isEqualB(int b1, int b2);
+bool isEqualGamma(int gamma1, int gamma2);
 bool isEqualColor(RGBa color1, RGBa color2);
 void BFill4Connected(FrameBuffer frame, Coordinate point, RGBa fillcoll, RGBa boundcol);
 void BFill8Connected(FrameBuffer frame, Coordinate point, RGBa fillcoll, RGBa boundcol);
